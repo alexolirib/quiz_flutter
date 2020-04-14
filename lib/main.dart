60fcc8bde5cb5cc6037d35afe9d2ecc2bf15211b
Alexandre Oliveira Ribeiro <alexolirib@gmail.com>
2020-04-13 23:03:50 -0300

    Exibindo lista de valores de Perguntas e respostas

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,55 @@ main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp>{
   var _perguntaIndex=0;
+  final _perguntas = const [
+      {
+        'Texto':"Qual é a sua cor favorita?",
+        'Resposta': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'Texto':"Qual é o seu animal favorito?",
+        'Resposta': ['Maria', 'João', 'Elefante', 'Leão']
+      },
+      {
+        'Texto':"Qual é o seu instrutor favorito?",
+        'Resposta': ['Alexandre', 'Leo', 'Jonas', 'Matheus']
+      },
+
+    ];
 
   void _responder(){
-    setState(() {
-      _perguntaIndex++;
-    });
-    print(_perguntaIndex);
+    if (temPerguntaSelecionada){
+      setState(() {
+        _perguntaIndex++;
+      });
+    }
     print('Pergunta respondida');
   }
+
+  //fazer getter
+  bool get temPerguntaSelecionada {
+    return _perguntaIndex < _perguntas.length;
+  }
+
   //preciso implementar um metodo para build
   @override
   Widget build(BuildContext context){
-    final perguntas = [
-      "Qual é a sua cor favorita?",
-      "Qual é o seu animal favorito?",
-    ];
+    
+
+    // var listResp = [];
+
+    // for (var resp in perguntas[_perguntaIndex]['Resposta'] ){
+    //   listResp.add(Resposta(resp, _responder));
+    // }
+
+
+    List<String> resp = temPerguntaSelecionada 
+      ? _perguntas[_perguntaIndex]['Resposta'] : null;
+    // List<Widget> mapResp = resp
+    //   .map((t)   => Resposta(t, _responder))
+    //   .toList();
+
+
     return MaterialApp(
       // home: Text('Olá meu amor!!'),
       //Scaffold vai dar estrutura da aplicação
@@ -40,21 +74,26 @@ class _PerguntaAppState extends State<PerguntaApp>{
         //     Text('linha3'),
         //   ],
        //com ctrl + "." 
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children:[
             // Text(perguntas[_perguntaIndex]),
-            Questao(perguntas[_perguntaIndex]),
+            Questao(_perguntas[_perguntaIndex]['Texto']),
             // RaisedButton(
             //   child: Text('Resposta1'),
             //   //aqui ta passando o metodo e não chamando
             //   onPressed: _responder
             // ),
-            Resposta('Resposta1', _responder),
-            Resposta('Resposta2', _responder),
-            Resposta('Resposta3', _responder),
-            
+
+            // primeira forma de mandar varios valores
+            // Resposta('Resposta1', _responder),
+            // Resposta('Resposta2', _responder),
+            // Resposta('Resposta3', _responder),
+            // segunda forma 
+            // ...listResp,
+            //terceira forma. Utilizando Map
+            ...resp.map((t)=>Resposta(t, _responder)).toList()
           ],
-        ),
+        ) : null,
         
       ),
     );
